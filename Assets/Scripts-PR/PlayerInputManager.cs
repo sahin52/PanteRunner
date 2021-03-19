a00997d6cee8320e09d2e71f66db1fec442cbfd4
sahin52 <sahinkasap52@gmail.com>
2021-03-19 22:43:00 +0300

    nearly done paintable cube

--- Assets/Scripts-PR/PlayerInputManager.cs
+++ Assets/Scripts-PR/PlayerInputManager.cs
@@ -8,8 +8,18 @@ public class PlayerInputManager : MonoBehaviour,IInputManager
     bool slide = false;
     float left_right = 0f;
 
+    Vector2 mouseDeltaPosition = Vector2.zero;
+    private Vector3 lastPos = Vector3.zero;
+
+    Touch touch;
+
     public bool jumpInput()
     {
+        if(jump == true)
+        {
+            jump = false;
+            return true;
+        }
         return jump;
     }
 
@@ -20,6 +30,11 @@ public class PlayerInputManager : MonoBehaviour,IInputManager
 
     public bool slideInput()
     {
+        if(slide == true)
+        {
+            slide = false;
+            return true;
+        }
         return slide;
     }
 
@@ -32,6 +47,74 @@ public class PlayerInputManager : MonoBehaviour,IInputManager
     // Update is called once per frame
     void Update()
     {
-        
+        SetMouseDeltaPos();
+        HandleLeftRight();
+        SetJump();
     }
+    private void SetJump()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            jump = true;
+            return;
+        }
+        //if()TODO
+
+
+
+
+    }
+    private void SetMouseDeltaPos()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            mouseDeltaPosition = Input.mousePosition - lastPos;
+
+            lastPos = Input.mousePosition;
+        }
+    }
+
+    private void HandleLeftRight()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            left_right = -1f;
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)){
+            left_right = 1f;
+            return;
+        }
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
+            {
+                left_right = touch.deltaPosition.x;
+            }
+            return;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            left_right = mouseDeltaPosition.x;
+            return;
+        }
+        left_right = 0f;
+        return;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
--- Assets/Scripts-PR/tempPaintScript.cs
+++ Assets/Scripts-PR/tempPaintScript.cs
@@ -1,27 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class tempPaintScript : MonoBehaviour
 {
     private Camera cam;
     public Texture2D filterTexture;
+    Color[] pixels;
     void Start()
     {
         cam = Camera.main;
         Clear();
+
     }
 
     void Update()
     {
         Paint();
+        print(CalculatePercentage());
     }
 
-
+    private float CalculatePercentage()
+    {
+        var len = pixels.Length;
+        int reds = 0;
+        for(int i = 0; i < len; i++)
+        {
+            if (pixels[i] == Color.red)
+            {
+                reds++;
+            }
+        }
+        return (float)reds / (float)len;
+        //throw new NotImplementedException();
+    }
 
     void Clear()
     {
         Renderer rend = GetComponent<Renderer>();
         Texture2D tex = rend.material.mainTexture as Texture2D;
-        var pixels = tex.GetPixels();
+        pixels = tex.GetPixels();
         var len = pixels.Length;
         for(int i = 0; i < len; i++)
         {
@@ -31,16 +48,28 @@ public class tempPaintScript : MonoBehaviour
         tex.Apply();
     }
 
-
+    Vector2 getInputPoint()
+    {
+        if (Input.touchCount > 0)
+        {
+            return Input.GetTouch(0).position;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            return Input.mousePosition;
+        }
+        return Vector2.zero;
+    }
     void Paint()
     {
-        if (Input.touchCount == 0)
+        var inputPoint = getInputPoint();
+        if(inputPoint == Vector2.zero)
+        {
             return;
-
+        }
         RaycastHit hit;
-        var touch = Input.GetTouch(0);
 
-        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+        if (!Physics.Raycast(cam.ScreenPointToRay(inputPoint), out hit))
         {
             print("didnt hit");
             return;
@@ -66,20 +95,45 @@ public class tempPaintScript : MonoBehaviour
         Vector2 pixelUV = hit.textureCoord;
         pixelUV.x *= tex.width;
         pixelUV.y *= tex.height;
-        paintAroundPixel(ref tex, ref pixelUV);
+        //paintPixel(tex, pixelUV);
+        PaintAroundPixel(ref tex, ref pixelUV);
+        //PaintWithFilter(tex,pixelUV);
     }
 
-    private void paintAroundPixel(ref Texture2D tex,ref  Vector2 pixelUV)
+    private void PaintWithFilter(Texture2D tex, Vector2 pixelUV)
     {
-        var pixels = tex.GetPixels();
-        var width = tex.width;
-        var height = tex.height;
 
-        for(int i = 0; i < filterTexture.width; i++)
+        //throw new NotImplementedException();
+    }
+
+    private void PaintAroundPixel(ref Texture2D tex,ref  Vector2 pixelUV)
+    {
+        print("Paint Arund Pixel");
+        pixels = tex.GetPixels();
+        
+        
+        var texWidth = tex.width;
+        var texHeight = tex.height;
+        var filterWidth = filterTexture.width;
+        var filterHeight = filterTexture.height;
+
+        var pixelX = (int)pixelUV.x;
+        var pixelY = (int)pixelUV.y;
+
+        var startingPos = pixelX-filterWidth;
+
+        for (int i = Mathf.Max(pixelX-filterWidth/2,0); i < Mathf.Min(pixelX -filterWidth/2+filterWidth,texWidth); i++)
         {
-            for(int j = 0; j < filterTexture.height; j++)
+            for(int j = Mathf.Max(pixelY - filterHeight/2,0); j < filterHeight + pixelY -filterHeight/2 && j<texHeight; j++)
             {
-                pixels[width * i + height] = Color.red;
+                //try
+                //{
+                pixels[texWidth * j + i] = Color.red;
+                //}
+                //catch (Exception)
+                //{
+
+                //}
             }
         }
         //for(int i =(int) Mathf.Clamp((int)pixelUV.x- filterTexture.width, 0, tex.width); i < Mathf.Clamp((int)pixelUV.x + filterTexture.width, 0, tex.width); i++)
